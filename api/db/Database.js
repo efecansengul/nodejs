c43cb3a42c39edc29c047c0a24b1a7c24b096a89
efecansengul <efecansengul33@gmail.com>
2025-04-25 22:53:13 +0300

    routing altyapısı kuruldu #2

---
+++ api/db/Database.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+let instance = null;
+
+class Database {
+  constructor() {
+    if (!instance) {
+      this.mongoConnection = null;
+      instance = this;
+    }
+    return instance;
+  }
+  async connect(options) {
+    try {
+      console.log("Db connecting...");
+      let db = await mongoose.connect(options.CONNECTION_STRING);
+      this.mongoConnection = db;
+      console.log("DB connectted");
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  }
+}
+
+module.exports = Database;
